fa1b9b901f2fa320517f1cd2fa821dcdc56eca8d
TanishSharmaTS <ttanishssharma@gmail.com>
2025-08-21 16:13:52 +0600

    Added JSON Schema and Schema Validation

--- LAB2/jsonParser.py
+++ LAB2/jsonParser.py
@@ -1,7 +1,12 @@
 import json
+import jsonschema
 import os
+from jsonschema import validate
 
 file_path = r"books.json"
+
+
+schema_file_path = r"bookSchema.json"
 
 
 def get_file_content(file_path):
@@ -16,25 +21,31 @@ def get_file_content(file_path):
         print("File not found!")
         return []
 
+def validate_json(data, schema):
+    try:
+        validate(instance = data, schema = schema)
+        print("Valid JSON")
+    except jsonschema.exceptions.ValidationError as e:
+        print("JSON is invalid")
 
 def print_books(content):
 
     print("\nBOOKS\n")
 
     for book in content:
-        print('ID: '+ book["id"])
+        print('ID: '+ str(book["id"]))
         print('Title: ' + book["title"])
         print('Author: ' + book["author"])
         print('Year Published: ' + book["yearPublished"])
-        print('Price: ' + book["price"] + "\n")
+        print('Price: ' + str(book["price"]) + "\n")
 
 
 def add_books(content, file_path):
-    ID = input("Enter book id\n")
+    ID = int(input("Enter book id\n"))
     title = input("Enter book title\n")
     author = input("Enter author name\n")
     yearPublished = input("Enter Year Published\n")
-    price = input("Enter price\n")
+    price = int(input("Enter price\n"))
 
     newBook = {
         'id': ID,
@@ -52,7 +63,7 @@ def add_books(content, file_path):
 
 def delete_books(content, file_path):
 
-    ID = input("Enter the book ID you want to delete\n")
+    ID = int(input("Enter the book ID you want to delete\n"))
 
     for book in content:
         if book.get('id') == ID:
@@ -67,7 +78,7 @@ def delete_books(content, file_path):
 
 def update_books(content, file_path):
 
-    edit_book = input("Enter the book id that you want to edit\n")
+    edit_book = int(input("Enter the book id that you want to edit\n"))
 
 
     for book in content:
@@ -75,13 +86,18 @@ def update_books(content, file_path):
             book['title'] = input("Enter the new Title\n") or book.get('title')
             book['author'] = input("Enter the new Author\n") or book.get('author')
             book['yearPublished']= input("Enter the new Year Published\n") or book.get('yearPublished')
-            book['price'] = input("Enter the new Price\n") or book.get('price')
+            new_price = input("Enter the new Price\n")
+            book['price'] = int(new_price) if new_price else book.get('price')
 
         with open(file_path, "w") as file:
             json.dump(content, file, indent = 4)
 
 
+content = get_file_content(file_path)
 
+schema = get_file_content(schema_file_path)
+
+validate_json(content, schema)
 
 while(True):
 
